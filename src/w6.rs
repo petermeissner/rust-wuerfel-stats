8f8c4f9d7d355b017febe08d3996064240f27003
Peter Meissner <retep.meissner@gmail.com>
2022-08-21 21:47:28 +0200

    documentation

--- src/w6.rs
+++ src/w6.rs
@@ -1,6 +1,17 @@
 use rand::Rng;
 
-
+/// Return random number from 6 sided dice (Wuürfel)
+/// 
+/// # Examples 
+/// 
+/// ```
+/// // get a single number
+/// let w = w6();
+/// 
+/// // print
+/// print!("One role of the dice: {}", w6());
+/// ```
+/// 
 fn w6() -> u8 {
   let x : u8 = rand::thread_rng().gen_range(1..7);
   return x;
